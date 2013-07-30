52d2577a2aad53dfc1b87bef699223ab75676217
Philip Hallstrom <philip@51shepherd.com>
2013-07-30 13:38:12 +0800

    stuff

--- spec/mailers/receipt_mailer_spec.rb
+++ spec/mailers/receipt_mailer_spec.rb
@@ -1,5 +1,13 @@
 require "spec_helper"
 
-describe ScannedReceiptNotifier do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe ReceiptMailer do
+  describe "new_scan_notification" do
+    let(:receipt) { mock_model(Receipt) }
+    let(:email) { ReceiptMailer.new_scan_notification(receipt).deliver }
+
+    # it { email.from.should == "from@example.com" }
+    # it { email.to.should == "philip@51shepherd.com" }
+    # it { email.subject.should == "New Scanned Receipt Available" }
+    # it { email.body.to_s.should match(/You uploaded a new receipt. This is what is it looks like:/) }
+  end
 end
